3ecc7ffc9e8bcaeaf7e635f83f0d427be796e86d
Thomas Bouranis <tbouranis@gmail.com>
2019-09-08 21:53:42 -0400

    Hello World

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import random
+
+upper = 100
+lower = 0
+x = random.randint(lower, upper)
+y = random.randint(lower, upper)
+sum = x + y
+avg = sum / 2
+
+print("Thomas Bouranis")
+print(x)
+print(y)
+print("Sum = {}".format(sum))
+print("Average = {}".format(avg))
